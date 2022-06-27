9d94dc3f42485d31b1cd8fd4ecaab7fbed85a9a6
daichi.suyama <symdit@gmail.com>
2022-06-27 11:24:12 +0900

    delete: user api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AppService } from './app.service'
-import { UserController } from './user/user.controller'
 import { PrismaService } from './prisma/prisma.service'
-import { UserService } from './user/user.service'
-import { CheckinController } from './checkin/checkin.controller';
-import { CheckinService } from './checkin/checkin.service';
+import { CheckinController } from './checkin/checkin.controller'
+import { CheckinService } from './checkin/checkin.service'
 
 @Module({
   imports: [],
-  controllers: [UserController, CheckinController],
-  providers: [AppService, PrismaService, UserService, CheckinService],
+  controllers: [CheckinController],
+  providers: [AppService, PrismaService, CheckinService],
 })
 export class AppModule {}
