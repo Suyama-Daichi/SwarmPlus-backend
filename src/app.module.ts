898022de68f2432354c14fdf1419644a667dcf41
daichi.suyama <symdit@gmail.com>
2022-07-02 10:28:23 +0900

    move directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { AppService } from './app.service'
-import { PrismaService } from './prisma/prisma.service'
+import { PrismaService } from './service/prisma/prisma.service'
 
 @Module({
   imports: [],
